8647df16cd4448ba951dce8fc1fb211cbf6bb5a2
Tim Nicholson <timrnicholson@gmail.com>
2024-01-23 16:30:10 -0600

    v1.4.5 PRO
    * Added the following PRO template parts and patterns:
         * Header Gradient Fixed
         * Header Scroll Fixed
         * Footer 3 Blocks with Gradient
         * Footer 3 Blocks with Curved Image
         * Footer 3 Blocks with Curved Image and Menu and Site Info
    * Updated CSS for:
         * Header with Scroll to Fixed for above
         * Navigation Menu block with dividers (not done yet though)

--- pro/inc/pro-patterns.php
+++ pro/inc/pro-patterns.php
@@ -16,18 +16,25 @@ add_filter('flatblocks_block_patterns', 'flatblocks_pro_block_patterns' );
 function flatblocks_pro_block_patterns( $block_patterns ) {
 
 	$pro_patterns = array(
-		'query-loop-3-columns' => array( 
-			'title' => __( 'Query Loop 3 Column Image Grid', 'flat-blocks' ),
+		'pro-footer-3-blocks-image-curved' => array( 
+			'title' => __( 'PRO Footer Curved Image w/3 Blocks', 'flat-blocks' ),
+			//'categories' => array ( 'flatblocks', 'footer' ),
+			'categories' => array ( 'flatblocks' ),
+			//'blockTypes' => array ('footer')
+			'inserter' => false
+		),
+		'pro-query-loop-3-columns' => array( 
+			'title' => __( 'PRO Query Loop 3 Column Image Grid', 'flat-blocks' ),
 			'categories' => array ( 'flatblocks', 'query' ),
 			'blockTypes' => array ('core/query')
 		),
-		'query-loop-colored-3-columns' => array( 
-			'title' => __( 'Query Loop 3 Column Colored Image Grid', 'flat-blocks' ),
+		'pro-query-loop-colored-3-columns' => array( 
+			'title' => __( 'PRO Query Loop 3 Column Colored Image Grid', 'flat-blocks' ),
 			'categories' => array ( 'flatblocks', 'query', 'featured' ),
 			'blockTypes' => array ('core/query')
 		)/*,
-		'query-loop-4-columns' => array( 
-			'title' => __( 'Query Loop 4 Column Image Grid', 'flat-blocks' ),
+		'pro-query-loop-4-columns' => array( 
+			'title' => __( 'PRO Query Loop 4 Column Image Grid', 'flat-blocks' ),
 			'categories' => array ( 'flatblocks', 'query' ),
 			'blockTypes' => array ('core/query')
 		)*/
