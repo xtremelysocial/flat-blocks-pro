1fe12057e7bb26fbec573ca2fe8d2f43dc0d6886
Tim Nicholson <timrnicholson@gmail.com>
2024-02-11 14:41:09 -0600

    v1.4.6 PRO
    * Updated sass-watch-pro.sh to watch both src/scss and pro/src/scss folders
    * Removed webpack files since they aren't currently being used

--- sass-watch-pro.sh
+++ sass-watch-pro.sh
@@ -1,3 +1,3 @@
-# Flat Blocks: Watch for updates to scss source and compile to pro/css
+# Flat Blocks PRO: Watch for updates to scss source and compile to pro/css
 #sass --watch --no-source-map pro/src/scss:pro/css --style compressed --scss
 sass --watch --no-source-map src/scss:assets/css pro/src/scss:pro/css 
