cb75e2d06ced660f7dad30b0038ea8d16f3a0175
Tim Nicholson <timrnicholson@gmail.com>
2024-02-10 19:32:39 -0600

    v1.4.6 PRO
    * Add sass-watch-pro.sh for devs to compile the source scss

---
+++ sass-watch-pro.sh
@@ -0,0 +1,3 @@
+# Flat Blocks: Watch for updates to scss source and compile to pro/css
+#sass --watch --no-source-map pro/src/scss:pro/css --style compressed --scss
+sass --watch --no-source-map pro/src/scss:pro/css
