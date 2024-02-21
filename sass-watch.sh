5f97594b302ae2068ce87799f6fb26dc45d8494c
Tim Nicholson <timrnicholson@gmail.com>
2024-02-21 15:17:43 -0600

    v1.4.7 Add Shell Scripts from PRO Version

---
+++ sass-watch.sh
@@ -0,0 +1,3 @@
+# Flat Blocks PRO: Watch for updates to scss source and compile to pro/css
+#sass --watch --no-source-map pro/src/scss:pro/css --style compressed --scss
+sass --watch --no-source-map src/scss:assets/css pro/src/scss:pro/assets/css 
