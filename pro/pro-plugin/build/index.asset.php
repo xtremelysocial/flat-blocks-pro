8e2d35813037066b49151d9a9c73f7522f4c0740
Tim Nicholson <timrnicholson@gmail.com>
2024-02-20 20:42:52 -0600

    v1.4.7 PRO
    * Uncompress view.js

--- pro/pro-plugin/build/index.asset.php
+++ pro/pro-plugin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-token-list'), 'version' => 'f785c177e0f71b6b788f');
+<?php return array('dependencies' => array(), 'version' => 'c62f4cf4887d19aef4e4');
