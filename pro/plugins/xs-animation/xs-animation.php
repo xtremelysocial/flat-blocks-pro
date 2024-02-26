d2b2b13dd611992cf8e021f2506dffed45ff2660
Tim Nicholson <timrnicholson@gmail.com>
2024-02-26 17:30:11 -0600

    v1.4.7 PRO
    * Update the plugin to use it's own Class and ensure only a single instance of it, whether its loaded from an actual plugin or as delivered in the PRO theme

--- pro/plugins/xs-animation/xs-animation.php
+++ pro/plugins/xs-animation/xs-animation.php
@@ -14,7 +14,7 @@
  *
  * @package           	xs-animation
  */
-
+ 
 /**
  * Animation & Visibility Plugin includes the following features, loaded in different ways
  * as follows:
@@ -29,86 +29,143 @@
  *
  */
 
-// Define our build directory
-define ('XS_ANIMATION_PLUGIN_ACTIVE', true);
-
-// Define our build directory
-define ('XSA_BUILD_DIR', __DIR__ . '/build/');
-
-// Define our plugin version from the build directory
-if ( file_exists ( XSA_BUILD_DIR . 'index.asset.php' ) ) {
-	$asset_file = include( XSA_BUILD_DIR . 'index.asset.php');
-	define ('XSA_VERSION', $asset_file['version']);
-} else {
-	define ('XSA_VERSION', '0.1.0');
+/* Don't allow this file to be loaded directly */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-/**
- * Load the pugin's CSS and Javascript on the back-end (only)
- */
-add_action( 'enqueue_block_editor_assets', 'xs_animation_load_block_editor_assets' );
+/* Plugin Class to handle instantiation and plugin functions */
+if ( ! class_exists( 'XS_Animation_Plugin' ) ) {
 
-function xs_animation_load_block_editor_assets() {
+	class XS_Animation_Plugin {
 
-	// Always load the plugin's main index.js. It will load all the individual
-	// feature's index.js files too.
-	if ( file_exists ( XSA_BUILD_DIR . 'index.asset.php' ) ) {
-		$asset_file = include( XSA_BUILD_DIR . 'index.asset.php');
-		wp_enqueue_script(
-			'flatblocks-pro-editor',
-			XSA_BUILD_DIR . 'index.js',
-			$asset_file['dependencies'],
-			$asset_file['version']
-			//XSA_VERSION
-		);
+		/**
+		 * Static property to hold our singleton instance
+		 *
+		 */
+		static $instance = false;
+
+
+		/**
+		 * Public properties to hold our plugin version and dependencies
+		 *
+		 * These will be filled later in __construct()
+		 *
+		 */	
+		public $version = '';
+
+		public $dependencies = '';
+	
+		/**
+		 * This is our constructor
+		 *
+		 * @return void
+		 */
+		private function __construct() {
+
+			// If we have an asset file, get the plugin version and dependencies 
+			// from there.
+			if ( file_exists ( __DIR__ . '/build/index.asset.php' ) ) {
+				$asset_file = include( __DIR__ . '/build/index.asset.php');
+				$this->version = $asset_file['version'];
+				$this->dependencies = $asset_file['dependencies'];
+			}
+
+			// Load the back end assets
+			add_action( 'plugins_loaded', array( $this, 'textdomain' ) );
+			add_action( 'enqueue_block_editor_assets', array( $this, 'xs_animation_load_block_editor_assets' ) );
+
+			// Load the front end assets
+			add_action( 'enqueue_block_assets', array( $this, 'xs_animation_load_block_assets' ) );
+			//add_action( 'wp_enqueue_scripts', array( $this, 'xs_animation_load_block_assets' ) );
+
+		}
+
+		/**
+		 * If an instance exists, this returns it.  If not, it creates one and
+		 * retuns it.
+		 *
+		 * @return XS_Animation_Plugin
+		 */
+		public static function getInstance() {
+			//var_dump('hello, world'); //TEST
+			if ( !self::$instance )
+				self::$instance = new self;
+			return self::$instance;
+		}
+
+		/**
+		 * Setup multiple language support 
+		 */
+		public function textdomain() {
+			load_plugin_textdomain( 
+				'xs-animation', 
+				false, 
+				dirname( plugin_basename( __FILE__ ) ) . '/languages/'
+			);
+		}
+
+		/**
+		 * Load the pugin's CSS and Javascript on the back-end (only)
+		 */
+		public function xs_animation_load_block_editor_assets() {
+			
+			if ( file_exists ( __DIR__ . '/build/index.js' ) ) {
+				wp_enqueue_script(
+					'xs-animation-editor',
+					__DIR__ . '/build/index.js',
+					$this->dependencies,
+					$this->version
+				);
+			}
+		
+			// If the plugin has an editor stylesheet, then load it
+			if ( file_exists ( __DIR__ . '/build/index.css' ) ) {
+				wp_enqueue_style(
+					'xs-animation-editor',
+					//XSA_BUILD_DIR . 'index.css',
+					__DIR__ . '/build/index.css',
+					array(),
+					$this->version
+				);
+			}		
+		}
+
+		/**
+		 * Load the plugin's Style CSS and Javascript on the front-end (only)
+		 */
+		public function xs_animation_load_block_assets() {
+
+			// Only do this for the front-end
+			if ( is_admin() ) {
+				return;
+			}
+		
+			// If the plugin has front-end javascript, then load it
+			if ( file_exists ( __DIR__ . '/build/view.js' ) ) {
+				wp_enqueue_script(
+					'xs-animation',
+					__DIR__ . '/build/view.js',
+					array('jquery'),
+					$this->version,
+					$in_footer = true
+				);
+			}
+		
+			// Load the plugin's main style-index.css
+			if ( file_exists ( __DIR__ . '/build/style-index.css' ) ) {
+				wp_enqueue_style(
+					'xs-animation',
+					__DIR__ . '/build/style-index.css',
+					array(),
+					$this->version,
+				);
+			}
+		}
+
+	/// end class
 	}
 
-	// If the plugin has an editor stylesheet, then load it
-	if ( file_exists ( XSA_BUILD_DIR . 'index.css' ) ) {
-		wp_enqueue_style(
-			'flatblocks-pro-editor',
-			XSA_BUILD_DIR . 'index.css',
-			array(),
-			//$asset_file['version']
-			XSA_VERSION
-		);
-	}
-}
-
-/**
- * Load the plugin's Style CSS and Javascript on the front-end (only)
- */
-add_action( 'enqueue_block_assets', 'xs_animation_load_block_assets' );
-
-function xs_animation_load_block_assets() {
-
-	/* Only do this for the front-end */
-	if ( is_admin() ) {
-		return;
-	}
-
-	// If the plugin has front-end javascript, then load it
-// 	if ( file_exists ( XSA_BUILD_DIR . 'view.asset.php' ) ) {
-	if ( file_exists ( XSA_BUILD_DIR . 'view.js' ) ) {
-// 		$asset_file = include( XSA_BUILD_DIR . 'view.asset.php');
-		wp_enqueue_script(
-			'flatblocks-pro',
-			XSA_BUILD_DIR . 'view.js',
-			array('jquery'),
-// 			$asset_file['dependencies'],
-// 			$asset_file['version']
-			XSA_VERSION,
-			$in_footer = true
-		);
-	}
-
-    // Load the plugin's main style-index.css
-	if ( file_exists ( XSA_BUILD_DIR . 'style-index.css' ) ) {
-		wp_enqueue_style(
-			'flatblocks-pro',
-			XSA_BUILD_DIR . 'style-index.css',
-			array(),
-			XSA_VERSION
-		);
-	}
-}
+	// Instantiate our class
+	$XS_Animation_Plugin = XS_Animation_Plugin::getInstance();
+}	
