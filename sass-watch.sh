963cda75b87ce5d402e95f2e56fc2b4448dda02d
Tim Nicholson <timrnicholson@gmail.com>
2024-03-16 16:46:27 -0500

    Delete sass-watch.sh

--- sass-watch.sh
+++
@@ -1,3 +0,0 @@
-# Flat Blocks: Watch for updates to scss source and compile to assets/css
-#sass --watch --no-source-map src/scss:assets/css --style compressed --scss
-sass --watch --no-source-map src/scss:assets/css
