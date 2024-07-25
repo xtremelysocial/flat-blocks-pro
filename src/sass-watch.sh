c9f46b051d447736bd1d7b1797cb621dec2c0d43
Tim Nicholson <timrnicholson@gmail.com>
2024-07-25 14:48:24 -0500

    v1.5 PRO

--- src/sass-watch.sh
+++ src/sass-watch.sh
@@ -1,4 +1,5 @@
 # Flat Blocks PRO: Watch for updates to scss source and compile to pro/css
 #!/bin/sh
 #sass --watch --no-source-map pro/src/scss:pro/css --style compressed --scss
-sass --watch --no-source-map src/scss:assets/css pro/src/scss:pro/assets/css 
+#sass --watch --no-source-map scss:/assets/css /pro/src/scss:/pro/assets/css 
+sass --watch --no-source-map ./scss:../assets/css ../pro/src/scss:../pro/assets/css
